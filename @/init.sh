21511ae27de53131348910ea59a95c6aae29f557
Windree <wings-@live.com>
2019-11-18 22:11:01 +0000

    Minecraft: publish status in #logs @irc

--- @/init.sh
+++ @/init.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 PORT=25565
 TIMEOUT=3600
-echo "Waiting for connections"
+{
+echo "Waiting for connections on port $PORT"
 nc -l -W 1 -p $PORT
+echo "Connection reserved. Starting minecraft" 
 bash -x /minecraft.sh "$PORT" "$TIMEOUT"
+echo "Connection dropped. Stopping minecraft" 
+} | /irc-send minecraft-server logs 
