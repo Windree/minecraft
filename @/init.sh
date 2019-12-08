4790d0966b743e123da957bc12e840bb394788b0
Windree <wings-@live.com>
2019-12-08 18:04:52 +0000

    Revert "Minecraft: switching to stable version of ubuntu"
    This reverts commit 1d97ac56c40d499dd40a82bbae338480c968fa2a.

--- @/init.sh
+++ @/init.sh
@@ -4,27 +4,75 @@ TIMEOUT=600
 BUILDTOOLS_URL="https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar"
 BUILDTOOLS_GIT="https://hub.spigotmc.org/stash/scm/spigot/buildtools.git"
 SPIGOT_GIT="https://hub.spigotmc.org/stash/scm/spigot/spigot.git"
-SERVER_PREFIX=/jar/
-DATA_PREFIX=/data/
-SERVER_EULA="$DATA_PREFIX/eula.txt"
-SERVER_PROPERTIES="$DATA_PREFIX/server.properties"
+
+set_motd(){
+	set_config "$1" "motd" "$2"
+}
+
+set_port(){
+	set_config "$1" "query.port" "$2"
+}
 
 set_config(){
 	sed -i -E "s/^(${2}=).*$/\1${3}/" "$1"
 }
 
-find_spigot(){
-	find $1 -maxdepth 1 -type f -name spigot-*.jar | tail -n 1
+get_server_path(){
+	find /jar/ -maxdepth 1 -type f -name spigot-*.jar | tail -n 1
+}
+
+get_connection_count(){
+	cat /proc/net/tcp | awk '$3 ~ /:63DD/' | wc -l
+}
+
+get_client_count(){
+	local tcp=$(get_connection_count "TCP" "$1")
+	local udp=0
+	echo $((tcp+udp))
+}
+
+prepare_server(){
+	echo 'eula=true' > "./eula.txt"
 }
 
 parse_version(){
 	echo "$1" | grep -oP '[\d.]+(?=\.)'
 }
 
-run(){
-	cd $2
-	local jar=$1
+get_serverconig(){
+	echo "./server.properties"
+}
 
+
+run(){
+	local jar=$1
+	local config=$(get_serverconig "$DATA")
+	echo jar $jar
+	version=$(parse_version "$jar")
+	if [ -f "$config" ]; then
+		set_port "$config" "$PORT"
+		set_motd "$config" "$version"
+	fi
+	java -XX:+UseG1GC -Xms1G -Xmx1G -jar "$jar" nogui --noconsole 2>&1 &
+	local PID=$!
+	echo "Server starting. PID: $PID"
+	echo "Wating for connections..."
+	sleep $TIMEOUT
+	local client_count=$(get_client_count "$PORT")
+	while : ; do
+		cat /proc/net/tcp
+		client_count=$(get_client_count "$PORT")
+		if [ "$client_count" -eq "1110" ]; then
+			echo "No connections..."
+			echo "Stopping server"
+			kill -s TERM $PID
+			wait $PID
+			echo "Server stopped"
+			exit 0
+		fi
+		echo [`date`] connections: $client_count
+		sleep 10
+	done
 }
 
 build(){
@@ -37,35 +85,16 @@ build(){
 	rm -rf /tmp/* 
 }
 
-
-jar=$(find_spigot "$SERVER_PREFIX")
+jar=$(get_server_path)
+ls -la /jar
 echo "Minecraft JAR: $jar"
-[ -z "$jar" ] && build && jar=$(find_spigot $SERVER_PREFIX)
+[ -z "$jar" ] && build
 echo "Waiting for connections on port $PORT"
-nc -l -W 1 -p $PORT 2>&1 > /dev/null
-echo "Connection reserved. Starting minecraft"
-set_config $SERVER_EULA "eula" "true"
-set_config $SERVER_PROPERTIES "motd" "$(parse_version $jar)"
-set_config $SERVER_PROPERTIES "query.port" $PORT
-(
-	cd $DATA_PREFIX
-	java -XX:+UseG1GC -Xms1G -Xmx1G -jar "$jar" nogui --noconsole 2>&1
-) &
-pid=$!
-echo "Minecraft PID: $pid"
-echo checking for conenction each 10 minutes
-sleep 600
-while : ; do
-	sleep 600
-	echo checking for conenction each 10 minutes
-	if lsof -i :$PORT -sTCP:ESTABLISHED; then
-		continue;
-	fi
-
-	echo "Connection dropped. Stopping minecraft" 
-	kill -15 $pid
-	wait
-	if [[ $(find "$jar" -mtime +7 -print) ]]; then
-	  build
-	fi
-done
+nc -l -W 1 -p $PORT
+echo "Connection reserved. Starting minecraft" 
+cd /data
+run "$jar"
+echo "Connection dropped. Stopping minecraft" 
+if [[ $(find "$jar" -mtime +7 -print) ]]; then
+  build
+fi
--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM ubuntu
+FROM ubuntu:18.10
 ARG DEBIAN_FRONTEND=noninteractive
-ARG PACKAGES="openjdk-11-jre-headless"
+ARG PACKAGES="openjdk-11-jre-headless netcat-openbsd"
 ARG BUILD_PACKAGES="git wget curl"
 RUN apt update && \
     apt install -y $PACKAGES $BUILD_PACKAGES && \
