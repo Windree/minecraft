3f8699b2217d6c3d573d2428becf097959b4be27
Windree <windree@dragonsoul.com>
2023-04-29 20:06:01 +0000

    feat:suppress the initial warning when not launched from a tty

--- backup.sh
+++ backup.sh
@@ -6,9 +6,9 @@ function main() {
     local root=$(get_path)
     local last_backup=$(find "$backup_path" -type f  -exec stat -c '%X %n' {} \; | sort -n | tail -1 | awk '{ print $2 }')
     if [ -z "$last_backup" ]; then
-        dar --create "$backup_path/full.dar" --fs-root "$root" --no-overwrite -zxz:1 -vt
+        dar --create "$backup_path/full.dar" --fs-root "$root" -Q --no-overwrite -zxz:1 -vt
     else
-        dar --create "$backup_path/incremental-$(date +%F-%T)" --ref "${last_backup%.*.*}" --fs-root "$root" --no-overwrite -zxz:1 -vt
+        dar --create "$backup_path/incremental-$(date +%F-%T)" --ref "${last_backup%.*.*}" --fs-root "$root" -Q --no-overwrite -zxz:1 -vt
     fi
 }
 
