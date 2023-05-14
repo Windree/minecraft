c3fc4f67e0b0d18c6b7fc52b411a3b0b03ead4e3
Windree <windree@dragonsoul.com>
2023-05-14 05:33:12 +0000

    feat: up docker containers on error

--- backup.sh
+++ backup.sh
@@ -1,28 +1,34 @@
 #!/bin/env bash
 set -Eeuo pipefail
 
+root=$(dirname "$(realpath $0)")
+
+trap 'up' EXIT
+
 function main() {
     local backup_path=$1
-    local root=$(get_path)
-    if ! docker-compose -f "$root/docker-compose.yml" stop; then
-        echo error stopping container
-        exit
-    fi
+
     local last_backup=$(find "$backup_path" -type f  -exec stat -c '%X %n' {} \; | sort -n | tail -1 | awk '{ print $2 }')
     if [ -z "$last_backup" ]; then
         dar --create "$backup_path/full.dar" --fs-root "$root" -Q --no-overwrite --compress=zstd -vt
     else
         dar --create "$backup_path/incremental-$(date +%F-%T)" --ref "${last_backup%.*.*}" --fs-root "$root" -Q --no-overwrite --compress=zstd -vt
     fi
+}
 
+up(){
     if ! docker-compose -f "$root/docker-compose.yml" up --build -d; then
-        echo error staring container
+        echo error starting container
         exit
     fi
 }
 
-function get_path() {
-    dirname "$(realpath $0)"
+down(){
+    if ! docker-compose -f "$root/docker-compose.yml" stop; then
+        echo error stopping container
+        exit
+    fi
 }
 
+down
 main "$1"
