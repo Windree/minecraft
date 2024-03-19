ccb89ea525334ae0aa28749db7e03aeeca83b3a5
Windree <windree@dragonsoul.com>
2024-03-19 14:47:18 +0000

    feat: upgrade to 1.20.4

--- image/_/build.sh
+++ image/_/build.sh
@@ -1,36 +1,51 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -Eeuo pipefail
 
 BUILDTOOLS_URL="https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar"
 BUILDTOOLS_GIT="https://hub.spigotmc.org/stash/scm/spigot/buildtools.git"
 SPIGOT_GIT="https://hub.spigotmc.org/stash/scm/spigot/spigot.git"
 
-jar(){
+root=$(mktemp -d)
+target=/spigot.jar
+version=/spigot.version
+
+function main() {
+	build
+	local jar=$(find_jar)
+	echo "Spigot: $jar"
+	ln -vs "$jar" "$target"
+	local version=$(parse_version "$jar")
+	echo "Version: $version"
+	echo -n "$version" >"$version"
+
+}
+
+function build() {
+	(
+		cd "$root" &&
+			curl -o build.jar "$BUILDTOOLS_URL" &&
+			java -jar build.jar --rev $MINECRAFT_VERSION &&
+			mv spigot-*.jar /
+	)
+}
+
+function find_jar() {
 	find / -maxdepth 1 -type f -name spigot-*.jar | tail -n 1
 }
 
-set_config(){
+function set_config() {
 	sed -i -E "s/^(${2}=).*$/\1${3}/" "$1"
 }
 
-parse_version(){
+function parse_version() {
 	echo "$1" | grep -oP '[\d.]+(?=\.)'
 }
 
-build(){
-	cd /tmp && \
-	curl -o build.jar "$BUILDTOOLS_URL" && \
-	java -jar build.jar --rev $MINECRAFT_VERSION && \
-	mkdir -p /jar && \
-	mv spigot-*.jar / && \
-	cd / && \
-	rm -rf /tmp/* 
+function cleanup() {
+	echo "Cleanup..."
+	rm -rfv "$root"
 }
 
-build
-jar=$(jar)
-echo "Spigot: $jar"
-ln -vs "$jar" "/spigot.jar"
-version=$(parse_version "$jar")
-echo "Version: $version"
-echo -n "$version" > /spigot.version
+trap cleanup exit
+
+main
