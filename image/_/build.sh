cf9ec959d0f558bebeabe4b3a13b9b920a219872
Windree <windree@dragonsoul.com>
2024-04-03 15:34:23 +0000

    fix: save version into file

--- image/_/build.sh
+++ image/_/build.sh
@@ -1,23 +1,22 @@
 #!/usr/bin/env bash
-set -Eeuo pipefail
+set -Eeuxo pipefail
 
 BUILDTOOLS_URL="https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar"
 BUILDTOOLS_GIT="https://hub.spigotmc.org/stash/scm/spigot/buildtools.git"
 SPIGOT_GIT="https://hub.spigotmc.org/stash/scm/spigot/spigot.git"
 
 root=$(mktemp -d)
-target=/spigot.jar
-version=/spigot.version
+target_jar=/spigot.jar
+version_file=/spigot.version
 
 function main() {
 	build
 	local jar=$(find_jar)
 	echo "Spigot: $jar"
-	ln -vs "$jar" "$target"
+	ln -vs "$jar" "$target_jar"
 	local version=$(parse_version "$jar")
 	echo "Version: $version"
-	echo -n "$version" >"$version"
-
+	echo -n "$version" >"$version_file"
 }
 
 function build() {
