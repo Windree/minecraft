61de7e8358263290abf4df83ed48614b0699a186
Windree <windree@dragonsoul.com>
2024-05-04 22:15:29 +0000

    refactor: use entrypoint script

--- image/_/entrypoint.sh
+++ image/_/entrypoint.sh
@@ -6,7 +6,7 @@ function main() {
 	set_config "eula.txt" "eula" "true"
 	set_config "server.properties" "motd" "$version"
 	# set_config "server.properties" "query.port" "$PORT"
-	java -XX:+UseG1GC -Xms6G -Xmx6G -jar "/spigot.jar" nogui --noconsole 2>&1
+	java -XX:+UseG1GC -Xms4G -Xmx4G -jar "/spigot.jar" nogui --noconsole 2>&1
 }
 
 set_config() {
