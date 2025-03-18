5efa9991eb64c61bde8003b314541f3916995877
Windree <windree@dragonsoul.com>
2025-03-18 18:53:34 +0000

    fix: get current script directory path

--- ufw.sh
+++ ufw.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-source "$(dirname "${BASH_SOURCE[0]}")/.env"
+declare root="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
+source "$root/.env"
 
 ufw allow proto tcp to any port $MINECRAFT_GAME_PORT
 ufw allow proto udp to any port $MINECRAFT_GAME_PORT
