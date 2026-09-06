ee7acc42c3f86030b2f142a42e3d43e569d1598f
Windree <windree@dragonsoul.com>
2026-09-06 19:27:21 +0000

    feat: remove obsolete map generation plugin

--- ufw.sh
+++ ufw.sh
@@ -4,5 +4,4 @@ source "$root/.env"
 
 ufw allow proto tcp to any port $MINECRAFT_GAME_PORT
 ufw allow proto udp to any port $MINECRAFT_GAME_PORT
-ufw allow proto tcp to any port $MINECRAFT_DYNMAP_PORT
 ufw reload
